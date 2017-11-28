0d9506f974cba20febc78b19888c3bfd62c7ef02
Harshal <harshal.carpenter@gmail.com>
2017-11-28 00:07:44 -0800

    User Creation/Get API

--- models/config.js
+++ models/config.js
@@ -1,3 +1,9 @@
 /**
  * Created by harshalcarpenter on 11/26/17.
  */
+
+var dbStr = "mongodb://heroku_h86dbp94:23lqki7gjgpht4q7u1h0ti9gae@ds147681.mlab.com:47681/heroku_h86dbp94";
+
+module.exports = {
+    "dbStr": dbStr
+};
